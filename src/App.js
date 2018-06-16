25feead533054cdbbaa09c5b502fc3d9b35116ea
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2018-06-16 16:31:35 -0400

    created working form and api requests

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import React from "react";
 import Titles from "./components/Titles"
 import Form from "./components/Form"
 import Display from "./components/Display"
+import Secrets from "./secrets.json"
+
+const API_KEY = Secrets.api_key;
+//http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <p><Titles /></p>
-        <p><Form /></p>
+        <p><Form getWeather={this.getWeather}/></p>
         <p><Display /></p>
       </div>
     );
