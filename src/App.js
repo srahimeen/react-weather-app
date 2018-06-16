4cd568dd028db0543506f97db00133a3f08b9f5d
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2018-06-16 16:00:25 -0400

    created all required components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Display from "./components/Display"
+
 class App extends React.Component {
   render() {
     return (
       <div>
-      <p>Hello World!</p>
+        <p><Titles /></p>
+        <p><Form /></p>
+        <p><Display /></p>
       </div>
     );
   }
